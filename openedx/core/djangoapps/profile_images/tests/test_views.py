30dba47877c8c2796a9775be38d9185097b7dc59
J. Cliff Dyer <cdyer@edx.org>
2015-10-06 21:00:43 +0000

    Silence unused-argument

--- openedx/core/djangoapps/profile_images/tests/test_views.py
+++ openedx/core/djangoapps/profile_images/tests/test_views.py
@@ -220,7 +220,7 @@ class ProfileImageViewPostTestCase(ProfileImageEndpointMixin, APITestCase):
         self.check_upload_event_emitted(old=TEST_UPLOAD_DT, new=TEST_UPLOAD_DT2)
 
     @patch('openedx.core.djangoapps.profile_images.views._make_upload_dt', return_value=TEST_UPLOAD_DT)
-    def test_upload_jpeg_mimetype(self, mock_make_image_version, mock_log):
+    def test_upload_jpeg_mimetype(self, mock_make_image_version, mock_log):  # pylint: disable=unused-argument
         """
         Test that a user can upload raw content with the appropriate mimetype
         """
